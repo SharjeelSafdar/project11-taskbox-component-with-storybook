384b94aa1ac8006fcbe1b8842f318205a1b5bb3f
Sharjeel Safdar <miansharjeelsafdar@gmail.com>
2020-12-07 23:43:01 +0500

    Added store context

--- src/components/app/App.stories.tsx
+++ src/components/app/App.stories.tsx
@@ -1,23 +1,27 @@
 import React from "react";
 import { Story, Meta } from "@storybook/react/types-6-0";
+import { Provider as ReduxProvider } from "react-redux";
 
 import { App } from "./App";
+import appStore from "../../store";
 
 export default {
   title: "Components/App",
   component: App,
   decorators: [
     (Story) => (
-      <div
-        style={{
-          backgroundColor: "#282c34",
-          padding: "1em",
-          display: "flex",
-          justifyContent: "center",
-        }}
-      >
-        <Story />
-      </div>
+      <ReduxProvider store={appStore}>
+        <div
+          style={{
+            backgroundColor: "#282c34",
+            padding: "1em",
+            display: "flex",
+            justifyContent: "center",
+          }}
+        >
+          <Story />
+        </div>
+      </ReduxProvider>
     ),
   ],
 } as Meta;
